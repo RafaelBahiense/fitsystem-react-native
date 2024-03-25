1c0d8cf6255207db3076e36fd24b3334ae6a0862
Rafael Bahiense <rbahiensebraptor@gmail.com>
2024-03-25 19:05:53 -0300

    feat: added react query boilerplate

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,10 +1,15 @@
 import { Slot } from "expo-router";
 import { SessionProvider } from "../contexts/authcontext";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function Root() {
+  const queryClient = new QueryClient();
+
   return (
-    <SessionProvider>
-      <Slot />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <Slot />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
